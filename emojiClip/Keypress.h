18fc2004dc4547e05471047891556d995e7c68ba
Factor101 <notahacker10001@gmail.com>
2021-12-01 09:48:52 -0500

    Added backspace, enter, and space detection. Improved Keypress class.

--- emojiClip/Keypress.h
+++ emojiClip/Keypress.h
@@ -4,49 +4,46 @@
 template <class T>
 class Keypress
 {
-private:
-	std::pair<INPUT, INPUT> key;
-	bool shouldShift;
-public:
-	static Keypress<T> shift;
-	Keypress(T keyCode, bool _shouldShift = false) 
-		: shouldShift(_shouldShift)
-	{
-		key.first.type = INPUT_KEYBOARD;
-		key.first.ki.wVk = (int)keyCode;
-		key.first.ki.dwFlags = 0;
-		key.second = key.first;
-		key.second.ki.dwFlags = KEYEVENTF_KEYUP;
-	}
-	void keystroke(bool doShift = false)
-	{
-		if(!doShift && shouldShift) { doShift = true; }
-		try {
-			if(doShift) shift.press();
+	private:
+		std::pair<INPUT, INPUT> key;
+		bool shouldShift;
+	public:
+		static Keypress<T> shift;
+
+		Keypress(T keyCode, bool _shouldShift = false) : shouldShift(_shouldShift)
+		{
+			key.first.type = INPUT_KEYBOARD;
+			key.first.ki.wVk = (int)keyCode;
+			key.first.ki.dwFlags = 0;
+			key.second = key.first;
+			key.second.ki.dwFlags = KEYEVENTF_KEYUP;
+		}
+
+		void keystroke(bool doShift = false) noexcept
+		{
+			if(!doShift && shouldShift) 
+				doShift = true;
+
+			if(doShift) 
+				shift.press();
+
 			SendInput(1, &key.first, sizeof(INPUT));
 			SendInput(1, &key.second, sizeof(INPUT));
-			if(doShift) shift.release();
-		} catch(std::string err) {
-			return;
+
+			if(doShift) 
+				shift.release();
 		}
-	}
-	void press()
-	{
-		try {
+
+		void press() noexcept
+		{
 			SendInput(1, &key.first, sizeof(INPUT));
-		} catch(std::string err) {
-			return;
 		}
-	}
-	void release()
-	{
-		try {
+
+		void release() noexcept
+		{
 			SendInput(1, &key.second, sizeof(INPUT));
-		} catch(std::string err) {
-			return;
 		}
-	}
 };
 
 template<class T>
-Keypress<T> Keypress<T>::shift = Keypress<T>( VK_LSHIFT, false );
+Keypress<T> Keypress<T>::shift = Keypress<T>(VK_LSHIFT, false);
